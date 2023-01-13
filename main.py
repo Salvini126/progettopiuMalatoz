26073f18dd3aa6f71efdb998163f1c6493d8ec1a
Salvini126 <salvini.matteo@iisgalvanimi.edu.it>
2023-01-13 11:58:34 +0000

    quasiCollegamento

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-#main.py
-# Import the Flask module that has been installed.
 from flask import Flask
-from flask import send_file
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+from flask_pymssql import pymssql  
+from flask import pandas as pd
 
-# Creating a new "app" by using the Flask constructor. Passes __name__ as a parameter.
+
 app = Flask(__name__)
 
+
+# Per rispondere alle chiamate cross origin
+CORS(app)
+
+conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='salvini.matteo', password='xxx123##', database='salvini.matteo')
 # Annotation that allows the function to be hit at the specific URL.
-@app.route("/books")
+@app.route("/")
 # Generic Python functino that returns "Hello world!"
-def books():
-    return send_file('books.json')
+def index():
+    return "Hello world!"
+
+# Questa route effettua una find() su tutto il DB (si limita ai primi 100 risultati)
+@app.route('/addresses', methods=['GET'])
+def get_all_addresses():
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+    for s in mil4326WKT.find().limit(100):
+        output.append(s['INDIRIZZO'])
+    return jsonify({'result': output})
+
+@app.route("/collegamneto")
+    def getConnnesione():
+      
+    df = pd.read_sql_query('select * from Docente', conn)
+    df
 
 # Checks to see if the name of the package is the run as the main package.
 if __name__ == "__main__":
