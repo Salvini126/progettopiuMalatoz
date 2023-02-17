db83ff88c40792d1268cb05017973baaf94e2ae0
ZhaoZhihengF <zhao.zhiheng@iisgalvanimi.edu.it>
2023-02-17 11:19:33 +0000

    boh

--- progettopiumalatoz/src/app/flaskLink.ts
+++ progettopiumalatoz/src/app/flaskLink.ts
@@ -1,6 +1,6 @@
 export class flaskLink {
     public static getUrl() : string {
-        return 'https://4200-salvini126-progettopium-7amb4hwi88h.ws-eu86.gitpod.io/'
+        return 'https://4200-salvini126-progettopium-7amb4hwi88h.ws-eu86.gitpod.io'
     }
 }
 
